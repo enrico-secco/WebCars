b7db99f72eb59f17c9e411b24bdcd69b63a6b207
Enrico <enricosecco@edu.unifil.br>
2024-03-02 00:07:22 -0300

    fix: Fix Firebase config

--- src/services/firebaseConnection.ts
+++ src/services/firebaseConnection.ts
@@ -3,14 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDG0X7473wthvtflR2O2n3wsLj04fNp7-E",
-  authDomain: "webcarros-1a8a7.firebaseapp.com",
-  projectId: "webcarros-1a8a7",
-  storageBucket: "webcarros-1a8a7.appspot.com",
-  messagingSenderId: "557180932456",
-  appId: "1:557180932456:web:2b75270164fc91790cc6dd",
-};
+const firebaseConfig = {};
 
 const app = initializeApp(firebaseConfig);
 
