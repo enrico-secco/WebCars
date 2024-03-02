07faedfd75959a034d418fe5a4103bedfedd7a16
Enrico <enricosecco@edu.unifil.br>
2024-03-02 00:07:22 -0300

    fix: Fix Firebase config

---
+++ src/services/firebaseConnection.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+const auth = getAuth(app);
+const storage = getStorage(app);
+
+export { db, auth, storage };
